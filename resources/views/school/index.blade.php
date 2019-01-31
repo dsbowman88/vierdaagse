7c1752671134e664037c5886d895981c2e383423
Alex Coenen <coenena21@gmail.com>
2019-01-31 15:28:18 +0100

    view show schools updated

--- resources/views/school/index.blade.php
+++ resources/views/school/index.blade.php
@@ -4,12 +4,19 @@
 <h1 class="">Scholen</h1>
 <div class="list-group">
 <ul>
-    <?php $schools=App\school::all() ?>
+    <?php
+        $schools=App\School::all()
+    ?>
+
     @foreach ($schools as $school)
+
     <li>
-        <a class="list-group-item list-group-item-action list-group-item-primary" href="/school/{{ $school->id }}">{{ $school->schoolname }}
+        <a class="list-group-item list-group-item-action list-group-item-primary"
+        href="/school/{{ $school->id }}">{{ $school->schoolname }}
+
         <span class="text-secondary">Contactpersoon: {{ $school->contact }}</span></a>
     </li>
+
     @endforeach
 </ul>
 </div>
