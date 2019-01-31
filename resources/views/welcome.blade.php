332b41b0a6448eb6589fadfee0b5e6bd7b1b3675
Alex Coenen <coenena21@gmail.com>
2019-01-31 12:29:31 +0100

    welcome updated, added links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Vierdaagse
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/tour">Tours</a>
+                    <a href="/school">Schools</a>
+                    <a href="/participant">Participants</a>
                 </div>
             </div>
         </div>
