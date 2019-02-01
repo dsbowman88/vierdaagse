345cd74837e15fd78415a720f0a3e043cb0b6cfe
Douglas Bowman <d.s.bowman88@gmail.com>
2019-02-01 12:37:05 +0100

    small UI updates

--- resources/views/participant/index.blade.php
+++ resources/views/participant/index.blade.php
@@ -7,7 +7,7 @@
     <?php $participants=App\Participant::all() ?>
     @foreach ($participants as $participant)
     <li>
-        <a class="list-group-item list-group-item-action list-group-item-primary" href="/participant/{{ $participant->id }}">{{ $participant->name }}
+        <a class="m-2 list-group-item list-group-item-action list-group-item-primary" href="/participant/{{ $participant->id }}">{{ $participant->name }}
         <span class="text-secondary">Afstand: {{ DB::table('tours')->where('id', $participant->tour_id)->value('distance') }} KM</span></a>
     </li>
     @endforeach
--- resources/views/tour/show.blade.php
+++ resources/views/tour/show.blade.php
@@ -8,8 +8,10 @@
 <h1>Tour: {{ $tour->distance }} KM</h1>
 <h3>Deelnemers:</h3> <br>
 @foreach ($participants as $participant)
-    <a class="list-group-item list-group-item-action list-group-item-primary" href="/participant/{{ $participant->id }}">{{ $participant->name }}
-    <span class="text-secondary">Afstand: {{ $tour->distance }} KM</span></a>
+    @if ($participant->tour_id == $tour->id)
+        <a class="m-2 list-group-item list-group-item-action list-group-item-primary" href="/participant/{{ $participant->id }}">{{ $participant->name }}
+        <span class="text-secondary">Afstand: {{ $tour->distance }} KM</span></a>
+    @endif
 @endforeach
 
 
